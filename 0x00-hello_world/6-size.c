1fe5835aeaa50b5932316bd694c76903a80f84f8
migjaz <mignotb1985@gmail.com>
2022-06-30 12:32:17 -0700

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,11 +12,11 @@ long c_long;
 long long c_long_long;
 float c_float;
 
-printf("Size of char: %d byte(s)\n", unsigned long sizeof(c_char));
-printf("Size of an int: %d byte(s)\n", unsigned long sizeof(c_int));
-printf("Size of a long int: %d byte(s)\n", unsigned long sizeof(c_long));
-printf("Size of a long long int: %d byte(s)\n", unsigned long sizeof(c_long_long));
-pritnf("Size of float: %d byte(s)\n", unsigned long sizeof(c_float));
+printf("Size of char: %d byte(s)\n", sizeof(char));
+printf("Size of an int: %d byte(s)\n", sizeof(int));
+printf("Size of a long int: %d byte(s)\n", sizeof(long));
+printf("Size of a long_long int: %d byte(s)\n", sizeof(long_long));
+pritnf("Size of float: %d byte(s)\n", sizeof(float));
 
 return (0);
 }
