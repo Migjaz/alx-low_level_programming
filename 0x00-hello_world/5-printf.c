36bfed7f44e547b37c215e4bd70a7ff7cba0d4e1
migjaz <mignotb1985@gmail.com>
2022-06-30 10:46:52 -0700

    5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,7 @@
 #include <studio.h>
+
 /**
  * main - Entry point
- *
- * Description: 'and that piece of art is useful - Dora Korpar, 2015-10-19'
- *
  * return:Always 0 (Success)
  */
 int main(void)
