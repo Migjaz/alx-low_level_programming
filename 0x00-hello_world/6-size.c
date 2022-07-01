cc7a1171ba14680934cd3148a55930e3b79331cf
migjaz <mignotb1985@gmail.com>
2022-07-01 07:21:48 -0700

    66

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,12 +6,17 @@
  */
 int main(void)
 {
+	int i;
+	char c;
+	long l;
+	double m;
+	float f;
 
-	printf("Size of char: %d byte(s)\n", sizeof(char));
-	printf("Size of an int: %d  byte(s)\n", sizeof(int));
-	printf("Size of a long int: %d  byte(s)\n", sizeof(long));
-	printf("Size of a long_long int: %d byte(s)\n", sizeof(double));
-	pritnf("Size of float: %d byte(s)\n", sizeof(float));
+	printf("Size of char: %lu. byte(s)\n", sizeof(c));
+	printf("Size of an int: %lu.  byte(s)\n", sizeof(i));
+	printf("Size of a long int: %lu.  byte(s)\n", sizeof(l));
+	printf("Size of a long_long int: %lu. byte(s)\n", sizeof(m));
+	pritnf("Size of float: %lu. byte(s)\n", sizeof(f));
 
 	return (0);
 }
