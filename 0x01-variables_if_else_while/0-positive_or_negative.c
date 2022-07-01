c80f1094e76dffe8fbd8adcfbf3ac1ad6354bb9c
migjaz <mignotb1985@gmail.com>
2022-07-01 08:29:11 -0700

    pos or neg

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,12 +5,13 @@
 
 int main(void)
 {
-	int n;
+	long n;
 	scanf(%lu);
-	if (n>0)
+	if (n>0);
 		printf("is positive.\n");
 	else (n==0);
 	printf("is zero.\n");
 	else (n<0);
 	printf("is negative.\n");
+	return (0);
 }
