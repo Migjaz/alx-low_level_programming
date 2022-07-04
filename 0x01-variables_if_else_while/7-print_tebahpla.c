57df1422b28e8616850f839321c26c0cf47157ed
migjaz <mignotb1985@gmail.com>
2022-07-04 03:26:38 -0700

    Z to A

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: a program that prints with pritf
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void);
+{
+int Z;
+for (Z = 'z'; Z >='a'; Z--)
+{
+putchar(Z);
+}
+putchar('\n');
+return (0);
+}
