1ba4262f839f7fbacd345a88b6dfcec80ae65eef
migjaz <mignotb1985@gmail.com>
2022-07-04 03:13:26 -0700

    1 -10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 if exited proprly
+ */
+
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+printf("%d", i);
+}
+printf("\n");
+return (0);
+}
